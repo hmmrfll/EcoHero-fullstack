bb0db3db56ea9c1ef9bd9a2ecebb2117a19871dd
hmmrfll <a.kazakevych@gmail.com>
2024-07-29 12:52:59 +0300

    first commit

--- tg-web-app-bot/src/backend.js
+++ tg-web-app-bot/src/backend.js
@@ -4,7 +4,7 @@ const userRoutes = require('./routes/userWeb')
 const cors = require('cors') // Добавить пакет cors
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 connectDB()
 
